fca41f341e480a4d73cfff97b13ebf129c616e33
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-07-11 20:07:10 +0200

    Fix _document to load fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const Document = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link href='https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap' rel='stylesheet' />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default Document
+export default MyDocument
