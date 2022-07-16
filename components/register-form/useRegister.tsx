e4a44c6f6cd1bc3f58d45d26678366179bd21557
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-07-17 01:31:02 +0200

    fix imports

--- components/register-form/useRegister.tsx
+++ components/register-form/useRegister.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from 'react'
 
 import { useUser } from '../../hooks/useUser'
+
 import { validateRegister } from './validateRegister'
 
 import type { RegisterValues } from '../../types/types'
